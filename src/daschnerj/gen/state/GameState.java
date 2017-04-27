1aed1a8dfe46f4f9f321051decbd5cab9409f7eb
Mr. Daschner <Daschner02@hotmail.com>
2017-04-27 01:14:17 -0400

    Fixed world loading location.

--- src/daschnerj/gen/state/GameState.java
+++ src/daschnerj/gen/state/GameState.java
@@ -12,7 +12,7 @@ public class GameState extends State {
 
 	public GameState(Handler handler) {
 		super(handler);
-		world = new World(handler, Utils.getDirectory() + "\\Worlds\\world1.txt");
+		world = new World(handler, Utils.getDirectory() + "\\Gen\\Worlds\\world1.txt");
 		handler.setWorld(world);
 
 	}
